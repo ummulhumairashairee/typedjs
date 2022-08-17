cfbfa54e60a3cef10374e9591f342aee0051f34f
ummulhumairashairee <103421186+ummulhumairashairee@users.noreply.github.com>
2022-08-17 21:00:07 +0600

    Add files via upload
    done

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function() {
+   
+
+    var typed = new Typed('#typingannimation', {
+        strings: ['Our Ambition', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiumod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco'],
+        typespeed:80,
+        smartBackspace: true // Default value
+      });
+    })
